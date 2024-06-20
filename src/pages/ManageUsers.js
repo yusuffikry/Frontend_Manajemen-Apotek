07bb921b7a9171ae411b315f8c93a23e8d4a96cd
yusuffikry <yusuffikri77@gmail.com>
2024-06-20 21:25:57 +0800

    Add ManageUsers page

---
+++ src/pages/ManageUsers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ManageUsersContainer = styled.div`
+  padding: 2rem;
+`;
+
+const ManageUsers = () => {
+  return (
+    <ManageUsersContainer>
+      <h2>Manage Users Page</h2>
+      {/* Your content for managing users */}
+    </ManageUsersContainer>
+  );
+};
+
+export default ManageUsers;
